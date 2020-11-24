ca149332b14f71563a7682b6359d70d49d070be7
Kushal <skushal094@gmail.com>
2020-11-25 00:11:52 +0530

    Made changes in the login page to support custom User model

--- chat_auth/admin.py
+++ chat_auth/admin.py
@@ -14,12 +14,12 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'admin')
+    list_display = ('email', 'admin', 'staff')
     list_filter = ('admin',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ()}),
-        ('Permissions', {'fields': ('admin',)}),
+        ('Permissions', {'fields': ('admin', 'staff')}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
