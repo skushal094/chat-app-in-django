1cd470b1fe09948c3a55ecec2e79d36eee73769a
Kushal <skushal094@gmail.com>
2020-12-20 19:30:12 +0530

    added url and template to logout the user

--- chat_auth/urls.py
+++ chat_auth/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="chat_auth/login.html", redirect_authenticated_user=True),
          name="login"),
     path('register/', views.SignUpView.as_view(), name="signup"),
+    path('logout/', LogoutView.as_view(template_name="chat_auth/logout.html"), name="logout"),
 ]
